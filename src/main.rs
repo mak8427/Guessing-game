96dfdb2b04f075cffff79715dd18cd1ce46c2619
mak8421 <davide.mattioli.2001@gmail.com>
2022-05-29 20:50:08 +0200

    init

--- src/main.rs
+++ src/main.rs
@@ -12,10 +12,10 @@ fn main() {
         io::stdin().read_line(&mut guess).expect("Failed to read line");
         let guess:u32 = guess.trim().parse().expect("type a number");
     match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Too small!"),
-        Ordering::Greater => println!("Too big!"),
+        Ordering::Less => println!("Too small"),
+        Ordering::Greater => println!("Too big"),
         Ordering::Equal => {
-            println!("You win!");
+            println!("You win");
             break;
         },
     }
