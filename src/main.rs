b9cca0f3c30df339f1f6feea56ddb4511b423cfc
mak8421 <davide.mattioli.2001@gmail.com>
2022-05-29 21:43:46 +0200

    Auto guesser

--- src/main.rs
+++ src/main.rs
@@ -4,21 +4,33 @@ use std::cmp::Ordering;
 
 fn main() {
     println!("Guess the number");
-    let secret_number=rand::thread_rng().gen_range(1..100);
-
+    let range = rand::thread_rng().gen_range(1..1000000);
+    let secret_number=rand::thread_rng().gen_range(1..range);
+    let mut guess= range/2;
+    let mut variator =range/2;
+    let mut times=0;
     loop{
         println!("input your guess");
-        let mut guess = String::new();
-        io::stdin().read_line(&mut guess).expect("Failed to read line");
-        let guess:u32 = guess.trim().parse().expect("type a number");
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Too small"),
-        Ordering::Greater => println!("Too big"),
+        match guess.cmp(&secret_number) {
+        Ordering::Less => {
+            println!("Too small");
+            guess=guess+variator;
+            variator=variator/2+1;
+            times=times+1
+        },
+        Ordering::Greater => {
+            println!("Too big");
+            guess=guess-variator;
+            variator=variator/2+1;
+            times=times+1
+        },
         Ordering::Equal => {
             println!("You win");
             break;
         },
-    }
+        }
+        println!("your guess {} the right one {}, times done are {} ", guess, secret_number, times);
+
     }
 
 }
