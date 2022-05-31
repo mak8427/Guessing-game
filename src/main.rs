7a77291478eaf36a93819e384c9f12417f314ba5
Davide mattioli <davide.mattioli.2001@gmail.com>
2022-05-31 12:16:48 +0200

    added plotting

--- src/main.rs
+++ src/main.rs
@@ -3,33 +3,33 @@ use std::cmp::Ordering;
 use plotters::prelude::*;
 
 
-fn main()-> Result<(), Box<dyn std::error::Error>>  {
-    const MAX: usize =10000;
-    let mut  array = vec![0; MAX];
+fn main() {
+    const MAX: usize =1000000;
+    const STEP: usize = 1;
+    let mut  array = vec![0; MAX/STEP];
     println!("Guess the number");
-    let range = rand::thread_rng().gen_range(1..100000000);
-
-    for n in (5..MAX).step_by(10) {
+    let mut i=0;
+    for n in (5..MAX).step_by(STEP) {
         let secret_number = rand::thread_rng().gen_range(1..n);
-        let mut guess = range / 2;
-        let mut variator = range / 2;
+        let mut guess = n / 2;
+        let mut variator = n / 2;
         let mut times = 0;
         loop {
             match guess.cmp(&secret_number) {
                 Ordering::Less => {
-                    guess = guess + variator;
-                    variator = variator / 2;
                     if variator == 0 {
                         variator = 1
                     }
+                    guess = guess + variator;
+                    variator = variator / 2;
                     times = times + 1
                 }
                 Ordering::Greater => {
-                    guess = guess - variator;
-                    variator = variator / 2;
                     if variator == 0 {
                         variator = 1
                     }
+                    guess = guess - variator;
+                    variator = variator / 2;
                     times = times + 1
                 }
                 Ordering::Equal => {
@@ -37,37 +37,25 @@ fn main()-> Result<(), Box<dyn std::error::Error>>  {
                 }
             }
         }
-        array[n]=times;
+        array[i]=times;
         println!("your guess {} the right one {}, times done are {}, variator {}, range is {}", guess, secret_number, times, variator, n);
+        i=i+1;
     }
+    let root_area = BitMapBackend::new("../Guessing-game/0.png", (640, 480)).into_drawing_area();
+    root_area.fill(&WHITE).unwrap();
 
-    let root = BitMapBackend::new("../rust_2/0.png", (640, 480)).into_drawing_area();
-    root.fill(&WHITE)?;
-    let mut chart = ChartBuilder::on(&root)
-        .caption("y=x^2", ("sans-serif", 50).into_font())
-        .margin(5)
-        .x_label_area_size(30)
-        .y_label_area_size(30)
-        .build_cartesian_2d(-1f32..1f32, -0.1f32..1f32)?;
+    let mut ctx = ChartBuilder::on(&root_area)
+        .set_label_area_size(LabelAreaPosition::Left, 40)
+        .set_label_area_size(LabelAreaPosition::Bottom, 40)
+        .caption("Line Plot Demo", ("sans-serif", 40))
+        .build_cartesian_2d(0..MAX, 0..100)
+        .unwrap();
 
-    chart.configure_mesh().draw()?;
+    ctx.configure_mesh().draw().unwrap();
 
-    chart
-        .draw_series(LineSeries::new(
-            (-50..=50).map(|x| x as f32 / 50.0).map(|x| (x, x * x)),
-            &RED,
-        ))?
-        .label("y = x^2")
-        .legend(|(x, y)| PathElement::new(vec![(x, y), (x + 20, y)], &RED));
-
-    chart
-        .configure_series_labels()
-        .background_style(&WHITE.mix(0.8))
-        .border_style(&BLACK)
-        .draw()?;
-
-    Ok(())
-
+    ctx.draw_series(
+        LineSeries::new((0..).step_by(STEP).zip(array.iter().map(|x|*x)), &GREEN)
+    ).unwrap();
 
 
 
