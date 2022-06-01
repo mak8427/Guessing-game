6d4a594e7bf02e8e85b4d69d3526b4a54df3fb93
mak8421 <davide.mattioli.2001@gmail.com>
2022-06-01 13:13:44 +0200

    Finished plottinh

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,29 @@ use std::cmp::Ordering;
 use plotters::prelude::*;
 
 
-fn main() {
-    const MAX: usize =1000000;
-    const STEP: usize = 1;
+fn sme(x:i32, arr:&Vec<i32>)->Vec<i32> {
+    let mut i=0;
+    let len_arr= arr.len();
+    let mut avg_arr = vec![0;len_arr-(x as usize)];
+    let mut sum=0;
+    while i<len_arr-(x as usize){
+        for h in i..i+(x as usize){
+            sum=sum+arr[h];
+        }
+    avg_arr[i]=sum/x;
+    sum=0;
+    i=i+1;
+    }
+    return avg_arr;
+}
+
+
+
+
+
+fn main()-> Result<(), Box<dyn std::error::Error>>{
+    const MAX: usize =1000000000;
+    const STEP: usize = 100000;
     let mut  array = vec![0; MAX/STEP];
     println!("Guess the number");
     let mut i=0;
@@ -41,6 +61,9 @@ fn main() {
         println!("your guess {} the right one {}, times done are {}, variator {}, range is {}", guess, secret_number, times, variator, n);
         i=i+1;
     }
+
+    let avg=sme(100,&array);
+
     let root_area = BitMapBackend::new("../Guessing-game/0.png", (640, 480)).into_drawing_area();
     root_area.fill(&WHITE).unwrap();
 
@@ -48,15 +71,18 @@ fn main() {
         .set_label_area_size(LabelAreaPosition::Left, 40)
         .set_label_area_size(LabelAreaPosition::Bottom, 40)
         .caption("Line Plot Demo", ("sans-serif", 40))
-        .build_cartesian_2d(0..MAX, 0..100)
+        .build_cartesian_2d(0..MAX, 0..50)
         .unwrap();
 
     ctx.configure_mesh().draw().unwrap();
 
     ctx.draw_series(
         LineSeries::new((0..).step_by(STEP).zip(array.iter().map(|x|*x)), &GREEN)
-    ).unwrap();
-
+    );
+    ctx.draw_series(
+            LineSeries::new((0..).step_by(STEP).zip(avg.iter().map(|x|*x)), &BLUE)
+    );
+        Ok(())
 
 
 
